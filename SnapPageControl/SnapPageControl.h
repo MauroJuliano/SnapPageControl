8d6d177785025a6983e44e4e1ce917067d1b5288
MauroJuliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-19 19:39:54 -0400

    Initial Commit

---
+++ SnapPageControl/SnapPageControl.h
@@ -0,0 +1,18 @@
+//
+//  SnapPageControl.h
+//  SnapPageControl
+//
+//  Created by user195713 on 7/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SnapPageControl.
+FOUNDATION_EXPORT double SnapPageControlVersionNumber;
+
+//! Project version string for SnapPageControl.
+FOUNDATION_EXPORT const unsigned char SnapPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapPageControl/PublicHeader.h>
+
+
